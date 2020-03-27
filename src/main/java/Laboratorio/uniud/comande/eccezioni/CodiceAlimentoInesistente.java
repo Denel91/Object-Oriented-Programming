3e1b525b76547d2d27c7554343f4787edd80c1cc
Daniele Di Salvo <disi91@hotmail.it>
2020-03-27 20:06:43 +0100

    Implemented class CodiceAlimentoInesistente

---
+++ src/main/java/Laboratorio/uniud/comande/eccezioni/CodiceAlimentoInesistente.java
@@ -0,0 +1,7 @@
+package Laboratorio.uniud.comande.eccezioni;
+
+/**
+ *
+ * Eccezione lanciata quando si richiede un alimento con un codice non conosciuto
+ */
+public class CodiceAlimentoInesistente extends Exception {}
