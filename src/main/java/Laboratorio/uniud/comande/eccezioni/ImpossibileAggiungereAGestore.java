a5847947075284005b32f5d65b0271c737b97b7a
Daniele Di Salvo <disi91@hotmail.it>
2020-03-27 20:07:28 +0100

    Implemented class ImpossibileAggiungereAGestore

---
+++ src/main/java/Laboratorio/uniud/comande/eccezioni/ImpossibileAggiungereAGestore.java
@@ -0,0 +1,7 @@
+package Laboratorio.uniud.comande.eccezioni;
+
+/**
+ * Eccezione generica nel caso di impossibilità di aggiunta di un
+ * nuovo elemento ad un gestore.
+ */
+public class ImpossibileAggiungereAGestore extends Exception {}
